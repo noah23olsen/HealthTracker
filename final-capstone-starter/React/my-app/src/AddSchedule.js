d325f81ac884a74e5bd963791e8617bd9144da8d
noah23olsen <noah23olsen@gmail.com>
2023-11-01 12:25:46 -0400

    datepicker added and reinstalled node modules

--- final-capstone-starter/React/my-app/src/AddSchedule.js
+++ final-capstone-starter/React/my-app/src/AddSchedule.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import api from './api';
+import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import TimePicker from 'react-time-picker';
 
 class AddSchedule extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     sleepTime: '',
-     wakeTime: ''
+      sleepTime: '',
+      wakeTime: '', // You can initialize with the desired default time
     };
   }
+
+  handleSleepTimeChange = (date) => {
+    this.setState({ sleepTime: date });
+  };
+
+  handleWakeTimeChange = (time) => {
+    this.setState({ wakeTime: time });
+  }
+  
 handleAddSchedule = (scheduleData) => {
     api.postSchedule(scheduleData)
    .then(response=>{
@@ -41,6 +53,13 @@ handleAddSchedule = (scheduleData) => {
             value={this.state.sleepTime}
             onChange={(e) => this.setState({sleepTime : e.target.value})}
             />
+          {/* <DatePicker
+              selected={this.state.sleepTime}
+              onChange={this.handleSleepTimeChange}
+              showTimeSelect
+              timeIntervals={15}
+              dateFormat="yyyy-MM-dd HH:mm:ss"
+            /> */}
         </div>
         <div>
           <label>Wake Time:</label>
@@ -49,6 +68,13 @@ handleAddSchedule = (scheduleData) => {
             value={this.wakeTime}
             onChange={(e) => this.setState({wakeTime : e.target.value})}
             />
+          {/* <DatePicker
+              selected={this.state.wakeTime}
+              onChange={this.handleSleepTimeChange}
+              showTimeSelect
+              timeIntervals={15}
+              dateFormat="yyyy-MM-dd HH:mm:ss"
+            /> */}
         </div>
         <button type="submit">Submit</button>
       </form> 
--- final-capstone-starter/React/my-app/src/App.js
+++ final-capstone-starter/React/my-app/src/App.js
@@ -1,4 +1,3 @@
-// import React, { useState, useEffect } from 'react';
 import './App.css';
 import UpdateSchedule from './UpdateSchedule';
 import AddSchedule from './AddSchedule';
@@ -6,23 +5,12 @@ import GetAllSchedules from './GetAllSchedules'
 import GetScheduleById from './GetScheduleById';
 import DeleteScheduleById from './DeleteScheduleById';
 import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-
 function App() {
   //Functional components with hooks(seen below) 
   //apparently this is the more 'modern' way to manage state 
   //we should change our projects over at some point 
-  // const [sleepId, setSleepId] = useState(''); // 
   
   return ( //note it returns only ONE element(similar to template in vue)
-    // <div className="App">
-    //   <h1>Sleep Schedule</h1>
-    //   <DeleteScheduleById />
-    //   <GetScheduleById />
-    //   <AddSchedule />
-    //   <br></br>
-    //   <GetAllSchedules />
-    //   <UpdateSchedule />
-    // </div>
     <Router>
       <div>
         <nav>
