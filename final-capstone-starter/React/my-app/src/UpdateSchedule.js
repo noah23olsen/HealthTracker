f5478f652bf2101f1f64fcde75b1639ac02aaef7
noah23olsen <noah23olsen@gmail.com>
2023-11-07 10:49:05 -0500

    UpdateSchedule changed to functional component

--- final-capstone-starter/React/my-app/src/UpdateSchedule.js
+++ final-capstone-starter/React/my-app/src/UpdateSchedule.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import api from './api';
 
-class UpdateSchedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sleepTime:'',
-      wakeTime:'',
-      sleepId:'',
-    };
-  }
+function UpdateSchedule(){
+    const [sleepTime, setSleepTime] = useState('');
+    const [wakeTime, setWakeTime] = useState('')
+    const [sleepId,setSleepId] = useState('')
 
-  handleUpdateSchedule = (schedule) => {
+  const handleUpdateSchedule = (schedule) => {
     console.log("schedule" +JSON.stringify(schedule, null, 2));
     api.updateSchedule(schedule)
     .then(response=>{
@@ -21,46 +16,45 @@ class UpdateSchedule extends Component {
       console.error(error)
     })
   };
-  handleSubmit2 = (e) => {
+  const handleSubmit2 = (e) => {
     console.log("reached handle submit 2")
     e.preventDefault();
 
     const updateSchedule = {
-      sleep_time: this.state.sleepTime,
-      wake_time: this.state.wakeTime,
-      id: this.state.sleepId
+      sleep_time: sleepTime,
+      wake_time: wakeTime,
+      id: sleepId
     }
-    this.handleUpdateSchedule(updateSchedule);
+    handleUpdateSchedule(updateSchedule);
 
   }  
 
-  render() {
     return (
       <div>
         <h2>Update Schedule</h2>
-      <form onSubmit={this.handleSubmit2}>
+      <form onSubmit={handleSubmit2}>
         <div>
           <label>Update Sleep Time:</label>
           <input
             type="text"
-            value={this.state.sleepTime}
-            onChange={(e) => this.setState({sleepTime: e.target.value})}
+            value={sleepTime}
+            onChange={(e) => setSleepTime(e.target.value)}
             />
         </div>
         <div>
           <label>Update Wake Time:</label>
           <input
             type="text"
-            value={this.state.wakeTime}
-            onChange={(e) => this.setState({wakeTime: e.target.value})}
+            value={wakeTime}
+            onChange={(e) => setWakeTime(e.target.value)}
             />
         </div>
         <div>
           <label>ID:</label>
           <input
             type="text"
-            value={this.state.sleepId}
-            onChange={(e) => this.setState({sleepId :e.target.value})}
+            value={sleepId}
+            onChange={(e) => setSleepId(e.target.value)}
             />
         </div>
         <button type="submit">Submit</button>
@@ -68,6 +62,5 @@ class UpdateSchedule extends Component {
       </div>
     );
   }
-}
 
 export default UpdateSchedule;
